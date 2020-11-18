311d2aefe6798b11ed2699a31cd090c13993c63d
Renat R. Dusaev <crank@qcrypt.org>
2020-11-18 20:32:42 +0700

    ~ daily on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-#def read(fname):
-#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "geode",
     version = "0.0.1",
@@ -17,15 +10,11 @@ setup(
     license = "MIT",
     keywords = "physics geometry gdml",
     url = "https://github.com/CrankOne/geode",
-    packages=['geode', 'geode.GDMLParser', 'geode.GDMLParser.v3_1_6'],
-    #package_dir={ 'geode': 'geode' },
+    packages=[ 'geode'
+             , 'geode.GDMLParser'
+             , 'geode.GDMLParser.v3_1_6'
+             ],
     package_data={ 'geode.GDMLParser.v3_1_6' : ['GDML_3_1_6/schema/*.xsd'] },
-    #long_description=read('README'),
-    #classifiers=[
-    #    "Development Status :: 3 - Alpha",
-    #    "Topic :: Utilities",
-    #    "License :: OSI Approved :: MIT License",
-    #],
     classifiers=[
         "Development Status :: 1 - Planning",
     ]
